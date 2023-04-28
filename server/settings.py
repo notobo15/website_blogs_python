3dae121934d6faa48c20363e8ba959bf4c784023
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2023-04-29 01:28:43 +0700

    login

--- server/settings.py
+++ server/settings.py
@@ -134,11 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'server/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'server/static')
 
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'server/static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'server/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
