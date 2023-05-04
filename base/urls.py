fee7cd21454bc32ad348f9e751dc00e0943e9a66
Nguyen Thanh Binh <69106519+notobo15@users.noreply.github.com>
2023-05-04 10:55:46 +0700

    update

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("logout/", views.logoutUser, name="logout"),
     path("register/", views.registerUser, name="register"),
     path("contact/", views.contact, name="contact"),
+    path("search/", views.search, name="search"),
     # path("like-article/", views.like_article, name='like-article'),
     path('create-article/', views.createArticle, name='create-article'),
     path('update-article/<str:id>/', views.updateArticle, name='update-article'),
@@ -17,7 +18,8 @@ urlpatterns = [
     path('<str:pk>/', views.category, name='category'),
 
     path('<str:pk>/<slug:detail>/', views.detail, name='detail'),
-    path("<str:pk>/<slug:detail>/like-article/", views.like_article, name='like-article'),
+    path("<str:pk>/<slug:detail>/like-article/",
+         views.like_article, name='like-article'),
 
 
 ]
